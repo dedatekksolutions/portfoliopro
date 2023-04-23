a166422b5e2eb01a823ada0e4a98775a4fb3b2d9
dedatekk <dedatekk@gmail.com>
2023-04-23 16:52:03 +1000

    14 render components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Home from "./PortfolioContainer/Home/Home";
+import PortfolioContainer from "./PortfolioContainer/PortfolioContainer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <PortfolioContainer />
     </div>
   );
 }
